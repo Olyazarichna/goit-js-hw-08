f0015120945a16e95ac3b50abae48fcb34cfd5c4
Olyazarichna <olya.zarichna@gmail.com>
2022-05-08 11:38:24 +0300

    task 1,2,3

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,29 @@
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+const listGallery = document.querySelector('.gallery');
+
+function markupListGallery() {
+  const markup = galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+      <li>
+    <a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a></li>`;
+    })
+    .join('');
+  listGallery.insertAdjacentHTML('beforeend', markup);
+}
+markupListGallery();
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+  showCounter: false,
+});
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,41 @@
+import throttle from 'lodash.throttle';
 
+const formEl = document.querySelector('form');
+const inputEl = document.querySelector('input');
+const textareaEl = document.querySelector('textarea');
+
+formEl.addEventListener('input', throttle(onFormInput, 500));
+formEl.addEventListener('submit', onFormSubmit);
+
+const KEY = 'feedback-form-state';
+
+onSaveInput();
+
+function onFormInput() {
+  let newMessage = {
+    email: '',
+    message: '',
+  };
+  newMessage.email = inputEl.value;
+  newMessage.message = textareaEl.value;
+
+  localStorage.setItem(KEY, JSON.stringify(newMessage));
+}
+
+
+
+function onSaveInput() {
+  const savedMessage = JSON.parse(localStorage.getItem(KEY));
+
+  if (savedMessage) {
+      inputEl.value = savedMessage.email;
+      textareaEl.value = savedMessage.message;
+    console.log(savedMessage);
+  }
+}
+function onFormSubmit(event) {
+  event.preventDefault();
+  console.log(JSON.parse(localStorage.getItem(KEY)));
+  localStorage.removeItem(KEY);
+  formEl.reset();
+}
